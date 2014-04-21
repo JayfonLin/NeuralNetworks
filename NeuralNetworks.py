a2927870a0e314be107280224ab878a94f22b83a
JayfonLin <linjiafang33@163.com>
2014-04-21 18:46:49 +0800

    稍微写了一下。。。

--- NeuralNetworks.py
+++ NeuralNetworks.py
@@ -1,40 +1,91 @@
 from numpy import *
+from math import exp
 import random
-thetas = []
+Theta1 = []
+Theta2 = []
+thetaVec = []
+mylamda = 10
 X = []
 y = []
-trainFile = open("train.csv", 'r')
-testFile = open("test.csv", 'r')
-submitFile = open("NESubmit.csv", 'w')
-trainLines = trainFile.readlines()
-trainFile.close()
-isFirst = True
-i = 0
-while i < 2:
+a = []
+def init():
+    
     j = 0
-    theta = []
-    while j < 385:
-        theta.append(random.uniform(-1e-4,1e-4))
+    tj = []
+    while j < 384:
+        k = 0
+        tk = []
+        while k < 385:
+            tk.append(random.uniform(-1e-4,1e-4))
+            k+=1
+        tj.append(tk)
         j+=1
-    thetas.append(theta)
-    i+=1
-print thetas
-i = -1
-for l in trainLines :
-    if isFirst :
-        isFirst = False
-        continue
-    i+=1
-    l = l.strip('\n')
-    xx = []
-    tokens = l.split(',')
-    print tokens[1]
-    y.append(float(tokens[1]))
-    ii = 1
-    while True:
-        xx.append(float(tokens[ii+1]))
-        ii+=1
-        if ii >= 385:
-            break
-    X.append(xx)
+    Theta1 = tj
+    
+    print Theta1
+    i = 0
+    ti = []
+    while i < 385:
+        ti.append(random.uniform(-1e-4,1e-4))
+        i+=1
+    Theta2 = ti
+    print Theta2
+    print 'initialize end'
+
+def readFile():
+    trainFile = open("train.csv", 'r')
+    testFile = open("test.csv", 'r')
+    submitFile = open("NNSubmit.csv", 'w')
+    trainLines = trainFile.readlines()
+    trainFile.close()
+    isFirst = True
+    i = -1
+    for l in trainLines :
+        if isFirst :
+            isFirst = False
+            continue
+        i+=1
+        l = l.strip('\r\n')
+        xx = []
+        tokens = l.split(',')
+        #print tokens[1]
+        y.append(float(tokens[1]))
+        ii = 1
+        while True:
+            xx.append(float(tokens[ii+1]))
+            ii+=1
+            if ii >= 385:
+                break
+        X.append(xx)
+    X = mat(X)
+    print 'read file end'
+
+def g(pz):
+    return 1.0/(1.0+exp(-pz))
+def J():
+    i = 0
+    while i < 10000:
+        
+def h(x):
+    i = 0
+    while i < 384:
+        j = 0
+        zz = 0.0
+        while j < 385:
+            zz += (Theta1[i][j]*x[j]
+            j+=1
+        aa = g(zz)
+        a.append(aa)
+        i+=1
+                   
+if __name__ == "__main__":
+    init()
+    readFile()
+    a.append(float(0.0))
+    print "End"
+
+
+
+
+
 
